4fe1ad369514222327e35c6df2c5d0732144780a
Adetunji Samuel <samadetunji01@gmail.com>
2024-12-07 15:38:59 +0000

    rename tests

--- content/video.py
+++ content/video.py
@@ -11,7 +11,22 @@ https://github.com/ytdl-org/youtube-dl/blob/master/README.md#how-can-i-detect-wh
 
 class Video(ContentBase):
 
+
     def is_valid_source(self):
+        pass
+
+    def download_video_from_url(self):
+        # download the video using ytdl
+        # return temp file path
+
+        # might need to use ffmpeg to convert the video to a format that can be used by wispher
+
+        # TODO : check if ydtcl can just auto download the audi
+        pass
+
+    def extract_audio_from_video(self):
+        # extract audio from the video
+        # return the audio file path
         pass
 
     def extract_content_from_source(self):
