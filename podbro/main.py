41e6969f0770396b31f183f3b7d653a2d17dd9d8
Adetunji Samuel <samadetunji01@gmail.com>
2024-12-26 02:02:53 +0000

    store main curr state

--- podbro/main.py
+++ podbro/main.py
@@ -25,38 +25,33 @@ Expected MVP:
 
 """
 
-from content_parsers.media import transcribe_audio_file, download_audio_file
+from podbro.content_parsers.media import transcribe_audio_file, download_file
 
-from content_parsers.webpage import is_valid_url
+from podbro.content_parsers.webpage import is_valid_url
 
-from content_parsers.pdf import file_exists, Pdf
-from content_parsers.youtube import YouTube
-from content_parsers.media import Media
-from content_parsers.webpage import WebPage
+from podbro.content_parsers.pdf import file_exists, Pdf
+from podbro.content_parsers.youtube import YouTube
+from podbro.content_parsers.media import Media
+from podbro.content_parsers.webpage import WebPage
 
-from generator.base import generate_podcast_transcript, parse_transcript
+from podbro.generator.base import parse_transcript, generate_podcast_transcript
 
-from tts.edge import EdgeSpeech
+from podbro.tts.edge import EdgeSpeech
+from podbro.tts.openai import OpenAISpeech
 
 import logging
-
-
-# TODO: Add cleanup method to remove unwanted files
-# Similar to Teardown in tests
-# TODO : make is valid source in base a static method or class method
 
 
 def speech_to_text():
     # big files should be handled
 
-    audio_file_path = download_audio_file("https://www.youtube.com/watch?v=VzJG4IpYDvs")
+    audio_file_path = download_file("https://www.youtube.com/watch?v=VzJG4IpYDvs")
     transcript = transcribe_audio_file(audio_file_path)
 
     return transcript
 
 
 def determine_content_parser_for_files(file_path):
-    raise "Not Implemented : need to adjust is valid source and test"
     # order is important
     content_parsers = (
         Pdf,
@@ -64,7 +59,7 @@ def determine_content_parser_for_files(file_path):
     )
 
     for content_parser in content_parsers:
-        if content_parser.is_valid_source(file_path):
+        if content_parser.validate_source(file_path):
             return content_parser(file_path)
 
     logging.error(f"No content parser found for the file:{file_path}")
@@ -72,7 +67,6 @@ def determine_content_parser_for_files(file_path):
 
 
 def determine_content_parser_for_urls(url):
-    raise "Not Implemented : need to adjust is valid source and test"
     # order is important
     content_parsers = (
         YouTube,
@@ -80,27 +74,23 @@ def determine_content_parser_for_urls(url):
         WebPage
     )
 
-    for content_parser in content_parsers:
-        if content_parser.is_valid_source(url):
-            return content_parser(url)
+    for parser in content_parsers:
+        if parser.validate_source(url):
+            return parser(url)
 
     logging.error(f"No content parser found for the url:{url}")
     raise Exception("No content parser found for the url")
 
 
-def create_podcast(
-        urls,  # webpages , video links  , regular youtube links ,  audio
-        text,  #
-        files  # audio , video , pdfs
-):
+def extract_content(urls, files, text):
     # validate weblinks
     for url in urls:
         if not is_valid_url(url):
-            raise Exception("Invalid URL")
+            raise Exception(f"Invalid URL : {url}")
 
     for file in files:
         if not file_exists(file):
-            raise Exception("Invalid FILE PATH")
+            raise Exception(f"Invalid FILE PATH :{file}")
 
     content = []
     if text:
@@ -115,9 +105,35 @@ def create_podcast(
         content.append(content_parser.extract_content_from_source())
 
     transcript = " ".join(content)
+    return transcript
+
+
+def get_tts(model_name="edge"):
+    tts_models = {
+        "edge": EdgeSpeech,
+        "openai": OpenAISpeech,
+    }
+
+    tts_model = tts_models.get(model_name)
+    if not tts_model:
+        raise Exception(f"No TTS model found for the model name:{model_name}")
+
+    return tts_model()
+
+
+def create_podcast(
+        urls,  # webpages , video links  , regular youtube links ,  audio
+        text,  #
+        files,  # audio , video , pdfs
+        content_model,  # openai ,
+        tts_model=None,  # edge , openai
+):
+    content = extract_content(urls, files, text)
+
+    transcript = generate_podcast_transcript(content)
     transcript_arr = parse_transcript(transcript)
 
-    speech = EdgeSpeech()
+    speech = get_tts(tts_model)
     _, result_file_path = speech.generate_audio_content(transcript_arr)
 
     return result_file_path
