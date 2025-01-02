66a624191d4837516b849f904c696d2b9545b4bf
Adetunji Samuel <samadetunji01@gmail.com>
2025-01-02 02:01:27 +0000

    fix: path issues in generator test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,12 +1,17 @@
 from unittest import TestCase
+import os
 
 from podbro.generator.base import parse_transcript, generate_podcast_transcript
 
 
 class Test(TestCase):
 
+    def setUp(self):
+        self.sample_txt_path = os.path.join(os.path.dirname(__file__), 'data', 'sample.txt')
+        self.sample_transcript_path = os.path.join(os.path.dirname(__file__), 'data', 'sample_transcript.txt')
+
     def test_generate_podcast_transcript_base(self):
-        with (open('./data/sample.txt', "r")) as file:
+        with (open(self.sample_txt_path, "r")) as file:
             content = file.read()
             transcript = generate_podcast_transcript(content)
             self.assertIsNotNone(transcript)
@@ -15,7 +20,7 @@ class Test(TestCase):
             self.assertTrue(("speaker 1" in transcript_lower) and ("speaker 2" in transcript_lower))
 
     def test_parse_transcript(self):
-        with (open('./data/sample_transcript.txt', "r")) as file:
+        with (open(self.sample_transcript_path, "r")) as file:
             content = file.read()
             transcript_arr = parse_transcript(content)
             self.assertIsInstance(transcript_arr, list)
